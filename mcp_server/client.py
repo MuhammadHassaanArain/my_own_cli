c27b6099c86d2a3ec5933f86168ae15cdd32c5c0
Muhammad Hassaan <rafatnaz21@gmail.com>
2025-11-21 12:02:33 +0500

    my own cli ✨

---
+++ mcp_server/client.py
@@ -0,0 +1,94 @@
+import json
+import asyncio
+from typing import Optional
+from mcp import ClientSession
+from contextlib import AsyncExitStack
+from mcp.client.streamable_http import streamablehttp_client
+from mcp.types import( ListToolsResult, CallToolResult, ListResourcesResult, 
+ListResourceTemplatesResult, ReadResourceResult, ListPromptsResult,  GetPromptResult)
+
+class MCPClient:
+    def __init__(self, server_url):
+        self._server_url : str = server_url
+        self._session: Optional[ClientSession] = None
+        self._exit_stack:AsyncExitStack= AsyncExitStack()
+
+    async def connection(self):
+        _read, _write, _ = await self._exit_stack.enter_async_context(
+            streamablehttp_client(self._server_url)
+        )
+        self._session = await self._exit_stack.enter_async_context(
+            ClientSession(_read,_write)
+        )
+        await self._session.initialize()
+        return self._session
+    
+    async def Cleanup(self):
+        await self._exit_stack.aclose()
+
+    async def __aenter__(self):
+        await self.connection()
+        return self
+    
+    async def __aexit__(self,*args):
+        await self.Cleanup()
+        self._session = None
+
+    # Tools
+    async def tool_list(self)-> ListToolsResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.list_tools()
+        return res.tools
+    
+    async def tool_call(self, tool_name:str, arguments:dict[str,any])->CallToolResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.call_tool(name=tool_name, arguments=arguments)
+        return res.content
+    
+    # Resources
+    async def list_resource(self)-> ListResourcesResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.list_resources()
+        return res.resources
+    async def list_template_res(self)-> ListResourceTemplatesResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.list_resource_templates()
+        return res.resourceTemplates
+    async def get_resource(self, uri:str)-> ReadResourceResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.read_resource(uri)
+        return res
+    # Prompts
+    async def list_prompt(self)-> ListPromptsResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.list_prompts()
+        return res
+    async def get_prompt(self, prompt_name:str)-> GetPromptResult:
+        assert self._session, "Session Not Found"
+        res = await self._session.get_prompt(name=prompt_name)
+        return res
+    
+
+async def main():
+    async with MCPClient("http://localhost:8000/mcp") as client:
+        print("-"*100)
+        tool_list = await client.tool_list()
+        for tool in tool_list:
+            print("Tools : ", tool.name)
+        call_Shell_tool = await client.tool_call(tool_name=tool.name, arguments={"cmd":"dir"})
+        raw_output = call_Shell_tool[0].text
+        shell_response = json.loads(raw_output)
+        cmd_output = shell_response.get("error") or shell_response.get("output")
+        print("Shell Tool Output : ", cmd_output)
+
+        call_read_file_tool = await client.tool_call(tool_name="read_files", arguments={"path":r"D:\Hassaan's Work\quarter_3_sdk\sdk_level_two_quiz_prep\linkedin_post_contribution.txt"})
+        print("Read File Tool Call: ", call_read_file_tool[0].text)
+
+        call_write_file_tool = await client.tool_call(tool_name="write_file", arguments={"path":r"D:\Hassaan's Work\quarter_4\self_study\my_own_cli\ai_native_app\mcp_server\README.md","content":"Hello written by tool"})
+        print("Write File Tool : ", call_write_file_tool[0].text)
+
+        call_list_dir_tool = await client.tool_call(tool_name="list_dir", arguments={"path":r"D:\Hassaan's Work\quarter_4\self_study\my_own_cli\ai_native_app\mcp_server"})
+        print("List Dir Tool : ", call_list_dir_tool[0].text)
+
+
+asyncio.run(main())
---
+++ mcp_server/server.py
@@ -0,0 +1,78 @@
+from  mcp.server.fastmcp import FastMCP
+import subprocess
+import os
+import aiofiles
+import asyncio
+
+mcp = FastMCP("MY-OWN-CLI-Server", stateless_http=True)
+
+@mcp.tool(name="run_shell")
+async def run_shell(cmd:str):
+    """Run a shell command and return output"""
+    try:
+        result = subprocess.check_output(cmd, shell=True,text=True)
+        return {"output":result}
+    except Exception as e:
+        return {"error":str(e)}
+
+@mcp.tool(name="read_files")
+async def read_file(path:str):
+    """Read a file asynchronousley and return its contents"""
+    if not os.path.exists(path):
+        return f"Error: Files not found -> {path}"
+    if os.path.isdir(path):
+        return f"Error: Path is a Directory, not a file -> {path}"
+    try:
+        async with aiofiles.open(path, "r", encoding="utf-8") as f:
+            content = await f.read()
+            return content
+    except Exception as e:
+        return f"Error reading file: {str(e)}"
+
+@mcp.tool(name="write_file")
+async def write_file(path:str, content:str):
+    """Create or overwrite a file with the given content using asyncio.to_thread 
+    so no external libraries are needed"""
+    folder = os.path.dirname(path)
+    if folder and not os.path.exists(folder):
+        try:
+            os.mkdir(folder, exist_ok=True)
+        except Exception as e:
+            return f"Error: Unable to create directories -> {str(e)}"
+    try:
+        def _write():
+            with open(path, "w", encoding="utf-8") as f:
+                f.write(content)
+            return "File written successfully!"
+        result = await asyncio.to_thread(_write)
+        return result
+    except Exception as e:
+        return f"Error writing file : {str(e)}"
+
+@mcp.tool(name="list_dir")
+async def list_dir(path:str):
+    """List the contents of a directory (files + folders)"""
+    if not os.path.exists(path):
+        return f"Error: Directory does not exist -> {path}"
+    if not os.path.isdir(path):
+        return f"Error: Path is not a directory -> {path}"
+    try:
+        def _list():
+            items = os.listdir(path)
+            if not items:
+                return "Directory is empty"
+            result = ["Contents:"]
+            for item in items:
+                full = os.path.join(path, item)
+                if os.path.isdir(full):
+                    result.append(f"[DIR] {item}")
+                else:
+                    result.append(f"[FILE] {item}")
+            return "\n".join(result)
+        return await asyncio.to_thread(_list)
+
+    except Exception as e:
+        return f"Error reading dirctory: {str(e)}"
+
+
+mcp_app = mcp.streamable_http_app()
